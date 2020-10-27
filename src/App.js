7d2bb7ff97d416f612f0fea0ed2ca411b332fac7
Shadab <shadab19it@gmail.com>
2020-10-27 10:40:46 +0530

    made top Scroll bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import TopScrollIndictor from "./ScrollTop";
 
 function App() {
+  const [topInde, setTopInde] = useState("0");
+  useEffect(() => {
+    const ScrollTop = () => {
+      const maxHeightPage = window.document.body.scrollHeight - window.innerHeight;
+      const scrollPercent = Math.round((window.scrollY / maxHeightPage) * 100);
+      setTopInde(scrollPercent);
+    };
+    window.addEventListener("scroll", ScrollTop);
+    return () => window.removeEventListener("scroll", ScrollTop);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div id='top' style={{ width: `${topInde}%` }} className='scrollIndicator'></div>
+      <h2>Lorem ipsum dolor sit amet.</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus cupiditate, nihil error fugiat voluptatum quae dicta, omnis illum
+        et temporibus quas nam id suscipit quo velit rem saepe dolore quibusdam dignissimos eius. Cum harum, esse accusantium nobis minima
+        illum explicabo sint, voluptas numquam laborum quidem blanditiis nam delectus deserunt cupiditate sunt iusto ad fugit voluptatem
+        quas id unde ducimus. Eius dicta quasi quos maxime corporis similique earum excepturi et repellat, possimus harum pariatur quia
+        perferendis modi tenetur, accusamus quibusdam? Amet illo odit omnis, magnam tempore, voluptatem placeat facere repellendus unde cum
+        voluptates dolore quaerat iste quae nobis enim animi magni.
+      </p>
+      <h2>Lorem ipsum dolor sit amet.</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus cupiditate, nihil error fugiat voluptatum quae dicta, omnis illum
+        et temporibus quas nam id suscipit quo velit rem saepe dolore quibusdam dignissimos eius. Cum harum, esse accusantium nobis minima
+        illum explicabo sint, voluptas numquam laborum quidem blanditiis nam delectus deserunt cupiditate sunt iusto ad fugit voluptatem
+        quas id unde ducimus. Eius dicta quasi quos maxime corporis similique earum excepturi et repellat, possimus harum pariatur quia
+        perferendis modi tenetur, accusamus quibusdam? Amet illo odit omnis, magnam tempore, voluptatem placeat facere repellendus unde cum
+        voluptates dolore quaerat iste quae nobis enim animi magni.
+      </p>
+      <h2>Lorem ipsum dolor sit amet.</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus cupiditate, nihil error fugiat voluptatum quae dicta, omnis illum
+        et temporibus quas nam id suscipit quo velit rem saepe dolore quibusdam dignissimos eius. Cum harum, esse accusantium nobis minima
+        illum explicabo sint, voluptas numquam laborum quidem blanditiis nam delectus deserunt cupiditate sunt iusto ad fugit voluptatem
+        quas id unde ducimus. Eius dicta quasi quos maxime corporis similique earum excepturi et repellat, possimus harum pariatur quia
+        perferendis modi tenetur, accusamus quibusdam? Amet illo odit omnis, magnam tempore, voluptatem placeat facere repellendus unde cum
+        voluptates dolore quaerat iste quae nobis enim animi magni.
+      </p>
+      <h2>Lorem ipsum dolor sit amet.</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus cupiditate, nihil error fugiat voluptatum quae dicta, omnis illum
+        et temporibus quas nam id suscipit quo velit rem saepe dolore quibusdam dignissimos eius. Cum harum, esse accusantium nobis minima
+        illum explicabo sint, voluptas numquam laborum quidem blanditiis nam delectus deserunt cupiditate sunt iusto ad fugit voluptatem
+        quas id unde ducimus. Eius dicta quasi quos maxime corporis similique earum excepturi et repellat, possimus harum pariatur quia
+        perferendis modi tenetur, accusamus quibusdam? Amet illo odit omnis, magnam tempore, voluptatem placeat facere repellendus unde cum
+        voluptates dolore quaerat iste quae nobis enim animi magni.
+      </p>
+      <h2>Lorem ipsum dolor sit amet.</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus cupiditate, nihil error fugiat voluptatum quae dicta, omnis illum
+        et temporibus quas nam id suscipit quo velit rem saepe dolore quibusdam dignissimos eius. Cum harum, esse accusantium nobis minima
+        illum explicabo sint, voluptas numquam laborum quidem blanditiis nam delectus deserunt cupiditate sunt iusto ad fugit voluptatem
+        quas id unde ducimus. Eius dicta quasi quos maxime corporis similique earum excepturi et repellat, possimus harum pariatur quia
+        perferendis modi tenetur, accusamus quibusdam? Amet illo odit omnis, magnam tempore, voluptatem placeat facere repellendus unde cum
+        voluptates dolore quaerat iste quae nobis enim animi magni.
+      </p>
+      <h2>Lorem ipsum dolor sit amet.</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus cupiditate, nihil error fugiat voluptatum quae dicta, omnis illum
+        et temporibus quas nam id suscipit quo velit rem saepe dolore quibusdam dignissimos eius. Cum harum, esse accusantium nobis minima
+        illum explicabo sint, voluptas numquam laborum quidem blanditiis nam delectus deserunt cupiditate sunt iusto ad fugit voluptatem
+        quas id unde ducimus. Eius dicta quasi quos maxime corporis similique earum excepturi et repellat, possimus harum pariatur quia
+        perferendis modi tenetur, accusamus quibusdam? Amet illo odit omnis, magnam tempore, voluptatem placeat facere repellendus unde cum
+        voluptates dolore quaerat iste quae nobis enim animi magni.
+      </p>
+      <h2>Lorem ipsum dolor sit amet.</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus cupiditate, nihil error fugiat voluptatum quae dicta, omnis illum
+        et temporibus quas nam id suscipit quo velit rem saepe dolore quibusdam dignissimos eius. Cum harum, esse accusantium nobis minima
+        illum explicabo sint, voluptas numquam laborum quidem blanditiis nam delectus deserunt cupiditate sunt iusto ad fugit voluptatem
+        quas id unde ducimus. Eius dicta quasi quos maxime corporis similique earum excepturi et repellat, possimus harum pariatur quia
+        perferendis modi tenetur, accusamus quibusdam? Amet illo odit omnis, magnam tempore, voluptatem placeat facere repellendus unde cum
+        voluptates dolore quaerat iste quae nobis enim animi magni.
+      </p>
+      <h2>Lorem ipsum dolor sit amet.</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus cupiditate, nihil error fugiat voluptatum quae dicta, omnis illum
+        et temporibus quas nam id suscipit quo velit rem saepe dolore quibusdam dignissimos eius. Cum harum, esse accusantium nobis minima
+        illum explicabo sint, voluptas numquam laborum quidem blanditiis nam delectus deserunt cupiditate sunt iusto ad fugit voluptatem
+        quas id unde ducimus. Eius dicta quasi quos maxime corporis similique earum excepturi et repellat, possimus harum pariatur quia
+        perferendis modi tenetur, accusamus quibusdam? Amet illo odit omnis, magnam tempore, voluptatem placeat facere repellendus unde cum
+        voluptates dolore quaerat iste quae nobis enim animi magni.
+      </p>
+      <h2>Lorem ipsum dolor sit amet.</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus cupiditate, nihil error fugiat voluptatum quae dicta, omnis illum
+        et temporibus quas nam id suscipit quo velit rem saepe dolore quibusdam dignissimos eius. Cum harum, esse accusantium nobis minima
+        illum explicabo sint, voluptas numquam laborum quidem blanditiis nam delectus deserunt cupiditate sunt iusto ad fugit voluptatem
+        quas id unde ducimus. Eius dicta quasi quos maxime corporis similique earum excepturi et repellat, possimus harum pariatur quia
+        perferendis modi tenetur, accusamus quibusdam? Amet illo odit omnis, magnam tempore, voluptatem placeat facere repellendus unde cum
+        voluptates dolore quaerat iste quae nobis enim animi magni.
+      </p>
+      <h2>Lorem ipsum dolor sit amet.</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus cupiditate, nihil error fugiat voluptatum quae dicta, omnis illum
+        et temporibus quas nam id suscipit quo velit rem saepe dolore quibusdam dignissimos eius. Cum harum, esse accusantium nobis minima
+        illum explicabo sint, voluptas numquam laborum quidem blanditiis nam delectus deserunt cupiditate sunt iusto ad fugit voluptatem
+        quas id unde ducimus. Eius dicta quasi quos maxime corporis similique earum excepturi et repellat, possimus harum pariatur quia
+        perferendis modi tenetur, accusamus quibusdam? Amet illo odit omnis, magnam tempore, voluptatem placeat facere repellendus unde cum
+        voluptates dolore quaerat iste quae nobis enim animi magni.
+      </p>
     </div>
   );
 }
